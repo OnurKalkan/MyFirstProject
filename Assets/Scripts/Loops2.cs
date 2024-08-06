14ad367dfce81756116bd5f71a4dd43e58b98325
OnurKalkan <onurkalkanv84@hotmail.com>
2024-08-06 23:04:53 +0300

    Lowest Health Finder

--- Assets/Scripts/Loops2.cs
+++ Assets/Scripts/Loops2.cs
@@ -5,6 +5,28 @@ using UnityEngine;
 public class Loops2 : MonoBehaviour
 {
     public int health = 100;
+    public GameObject cubeMan;
+    public GameObject[] cubePersons;
+
+    private void Start()
+    {
+        //print(cubePersons[6].transform.GetChild(0).name);
+        string minHealthChar = "";
+        int minHealth = 2000;
+        for (int i = 0; i < cubePersons.Length; i++)
+        {
+            if (cubePersons[i].GetComponent<CubePerson>().health < minHealth)
+            {
+                minHealthChar = cubePersons[i].name;
+                minHealth = cubePersons[i].GetComponent<CubePerson>().health;
+            }                
+        }
+        for (int i = 0; i < cubePersons.Length; i++)
+        {
+            if (cubePersons[i].name == minHealthChar)
+                print("Lowest health character: " + cubePersons[i].name);
+        }
+    }
 
     private void Update()
     {
