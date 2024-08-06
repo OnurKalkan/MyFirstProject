c281353749ac6dc870e2ed947176203289194bae
OnurKalkan <onurkalkanv84@hotmail.com>
2024-08-06 21:54:15 +0300

    Loops2 koduna fonksiyonellik eklendi

--- Assets/Scripts/Loops2.cs
+++ Assets/Scripts/Loops2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Loops2 : MonoBehaviour
 {
     public int health = 100;
 
@@ -10,22 +10,27 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            GameObject[] cubePersons = GameObject.FindGameObjectsWithTag("CubePerson");
-            for (int x = 0; x < 7; x++)
+            BuyuAtma();
+        }
+    }
+
+    public void BuyuAtma()
+    {
+        GameObject[] cubePersons = GameObject.FindGameObjectsWithTag("CubePerson");
+        for (int x = 0; x < 7; x++)
+        {
+            cubePersons[x].GetComponent<CubePerson>().health -= 10;
+            if (cubePersons[x].GetComponent<CubePerson>().health <= 0)
             {
-                cubePersons[x].GetComponent<CubePerson>().health -= 10;
-                if(cubePersons[x].GetComponent<CubePerson>().health <= 0)
+                cubePersons[x].GetComponent<Rigidbody>().useGravity = true;
+                cubePersons[x].GetComponent<Rigidbody>().isKinematic = false;
+                int y = 0;
+                while (y < 5)
                 {
-                    cubePersons[x].GetComponent<Rigidbody>().useGravity = true;
-                    cubePersons[x].GetComponent<Rigidbody>().isKinematic = false;
-                    int y = 0;
-                    while (y < 5)
-                    {
-                        cubePersons[x].transform.GetChild(y).GetComponent<Rigidbody>().useGravity = true;
-                        cubePersons[x].transform.GetChild(y).GetComponent<Rigidbody>().isKinematic = false;
-                        y++;
-                    }
-                }                
+                    cubePersons[x].transform.GetChild(y).GetComponent<Rigidbody>().useGravity = true;
+                    cubePersons[x].transform.GetChild(y).GetComponent<Rigidbody>().isKinematic = false;
+                    y++;
+                }
             }
         }
     }
